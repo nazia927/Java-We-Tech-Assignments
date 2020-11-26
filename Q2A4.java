1d51ab0a08a2534e1de0f87fc8a07d148bdb660e
nazia927 <64599191+nazia927@users.noreply.github.com>
2020-11-26 22:21:12 +0400

    Add files via upload

--- Q2A4.java
+++ Q2A4.java
@@ -1,10 +1,21 @@
-public class Answer2 {
-    public static void main(String[] args) {
-        int var1 = -1;
-        byte var2 = (byte) var1;
-        char var3 = (char) var2;
-        int var4 = (int) var3;
-        System.out.println(var4);
+public class Q2A4 {
 
-    }
-}
+	public static void main(String[] args) {
+		int int_value = -1;
+		byte byte_value = (byte)int_value;
+		char char_value = (char)byte_value;
+		int final_int_value = (int)char_value;
+		if(int_value == final_int_value)
+		{
+			System.out.println("Same Values");
+		}
+		else
+		{
+			System.out.println("Different Values");
+		}
+		
+		
+		System.out.println(int_value);
+		System.out.println(byte_value);
+		System.out.println(char_value);
+		System.out.println(final_int_value);
